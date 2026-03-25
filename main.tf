53277c32e096c198796a4b1c9d9ff3c0f00f5f6b
[Nabil Qureshi] <[nabilqureshi446@gmail.com]>
2026-03-25 17:00:24 +0530

    fix

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+    region = "us-east-1"
+  
+}
+
+module "rds"{
+  source = "./modules/RDS"
+  project = "cbz"
+  environment = "dev"
+  allocated_storage = 20
+  max_allocated_storage = 100
+  instance_class = "db.t3.micro"
+  username = "admin"
+  password = "password123"
+} 
+  
+
+module "eks" {
+
+  source = "./modules/EKS"
+  project = "cbz"
+  environment = "dev"
+    desired_nodes = 3
+    max_nodes = 10
+    min_nodes = 1
+    node_instance_type = "t3.medium"
+
+}
+
+module "s3" {
+    source = "./modules/S3"
+    environment = "dev"
+    bucket_name = "cbz-eks-bucket"
+
+  
+}
